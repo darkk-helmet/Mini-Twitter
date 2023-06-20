bd369dc65047a15cce56c58b8218cf32f945634f
Marty Mitchell <martyandmojo@icloud.com>
2023-06-20 15:49:21 -0700

    Removed redundant function

--- src/window/type/DialogWindow.java
+++ src/window/type/DialogWindow.java
@@ -16,39 +16,33 @@ public class DialogWindow extends JDialog {
 
         buttonOK.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                onOK();
+                onOKOrCancel();
             }
         });
 
         buttonCancel.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                onCancel();
+                onOKOrCancel();
             }
         });
 
-        // call onCancel() when cross is clicked
+        // call onOKOrCancel() when cross is clicked
         setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
         addWindowListener(new WindowAdapter() {
             public void windowClosing(WindowEvent e) {
-                onCancel();
+                onOKOrCancel();
             }
         });
 
-        // call onCancel() on ESCAPE
+        // call onOKOrCancel() on ESCAPE
         contentPane.registerKeyboardAction(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                onCancel();
+                onOKOrCancel();
             }
         }, KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);
     }
 
-    private void onOK() {
-        // add your code here
-        dispose();
-    }
-
-    private void onCancel() {
-        // add your code here if necessary
+    private void onOKOrCancel() {
         dispose();
     }
 
